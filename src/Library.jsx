0d7055d7f715e85cec9f4780feb2791d2a4e59b8
nikelborm <r0075301net.r@yandex.ru>
2021-02-06 01:57:41 +0300

    Finished work? We need more examples

--- src/Library.jsx
+++ src/Library.jsx
@@ -19,6 +19,29 @@ export const isValuesOnLinksEqual = links => ( prevAppStateContainer, nextAppSta
     return true;
 };
 
+function update( what ) {
+    return Object.prototype.toString.call( what ) === '[object Array]' ? [ ...what ] : { ...what };
+}
+
+export function setValueOnLink( treein, link, value ) {
+    let growingLink = [];
+    const tree = update( treein );
+    for ( const currentNodeName of link.slice( 0, -1 ) ) {
+        let parentNode = tree;
+        for ( const nodeName of growingLink ) {
+            parentNode = parentNode[ nodeName ];
+        }
+        parentNode[ currentNodeName ] = update( parentNode[ currentNodeName ] );
+        growingLink.push( currentNodeName );
+    }
+    let parentNode = tree;
+    for ( const nodeName of growingLink ) {
+        parentNode = parentNode[ nodeName ];
+    }
+    parentNode[ link[ link.length - 1 ] ] = value;
+    return tree;
+}
+
 export const getAppStateAndActionsConsumer = Context =>
 ( { children } ) => (
     <Context.Consumer>
@@ -28,26 +51,27 @@ export const getAppStateAndActionsConsumer = Context =>
     </Context.Consumer>
 );
 
-export const getGeneratorOfAppStateAndActionsConsumersThatRerendersOnlyWhenValuesOnLinksChanged = Context => links =>
-( { children } ) => (
-    <Context.Consumer>
-        { ( { appState, stateDependentActions } ) => (
-            React.createElement(
-                React.memo(
-                    // @ts-ignore
-                    () => children( appState, stateDependentActions ),
-                    isValuesOnLinksEqual( links )
-                ),
-                { appState, stateDependentActions }
-            )
-        ) }
-    </Context.Consumer>
-);
+export const getGeneratorOfAppStateAndActionsConsumersThatRerendersOnlyWhenValuesOnLinksChanged = Context => links => {
+    const Wrapper = React.memo(
+        // @ts-ignore
+        ( { children, appState, stateDependentActions } ) => children( appState, stateDependentActions ),
+        isValuesOnLinksEqual( links )
+    )
+    return ( { children } ) => (
+        <Context.Consumer>
+            { ( { appState, stateDependentActions } ) => (
+                React.createElement(
+                    Wrapper,
+                    { appState, stateDependentActions, children }
+                )
+            ) }
+        </Context.Consumer>
+    )
+};
 
-export const getGeneratorOfComponentsSubscribedForLinks = Context => ( Component, links ) =>
-props => {
+export const getGeneratorOfComponentsSubscribedForLinks = Context => ( Component, links ) => {
     const SomeShit = getGeneratorOfAppStateAndActionsConsumersThatRerendersOnlyWhenValuesOnLinksChanged( Context )( links );
-    return (
+    return props => (
         <SomeShit
             children={
                 ( appState, stateDependentActions ) => React.createElement( Component, {...props, appState, stateDependentActions } )
@@ -58,13 +82,8 @@ props => {
 
 export const getAppStateAndActionsProvider = Context => (
 class extends Component {
-    setValueStoredOnLink = ( link ) => {};
-    updateDataOnLink = ( link, calback ) => {
-        this.setState( prevState => {
-            calback( getValueFromLink( prevState, link ) )
-            return prevState;
-        });
-    }
+    setStateNode = ( link, value ) => this.setState( prevState => setValueOnLink( prevState, link, value ) );
+    getStateNode = link => getValueFromLink( this.state, link );
     actions = {};
     state = {};
     render() {
